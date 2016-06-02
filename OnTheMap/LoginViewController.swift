283eea1bdc337dd833a3dc59503c02b29ba7ba8e
Ramiro H Lopez <ramiro.h.lopez@outlook.com>
2016-06-01 22:23:52 -0700

    Add activity indicator when logging in Edit text field inset

--- OnTheMap/LoginViewController.swift
+++ OnTheMap/LoginViewController.swift
@@ -14,12 +14,20 @@ class LoginViewController: UIViewController {
     @IBOutlet weak var passwordTextField: UITextField!
     @IBOutlet weak var loginButton: UIButton!
     @IBOutlet weak var signUpButton: UIButton!
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
     
+    
+    override func viewDidLoad() {
+        setUpTextField(emailTextField)
+        setUpTextField(passwordTextField)
+    }
     
     @IBAction func loginButtonPressed(sender: AnyObject) {
+        activityIndicator.startAnimating()
         API.sharedInstance().udacityLogin(emailTextField.text!, password: passwordTextField.text!) { (success, errorString) in
             dispatch_async(dispatch_get_main_queue(), { 
                 if success {
+                    self.activityIndicator.stopAnimating()
                     self.completeLogin()
                 } else {
                     print("Unable to login")
@@ -33,4 +41,17 @@ class LoginViewController: UIViewController {
         presentViewController(masterTabController, animated: true, completion: nil)
     }
     
+    func setUpTextField(textField: UITextField) {
+        let padding = UIView(frame: CGRectMake(0,0,15,textField.frame.height))
+        textField.leftView = padding
+        textField.leftViewMode = UITextFieldViewMode.Always
+        textField.delegate = self
+    }
+    
+}
+
+extension LoginViewController: UITextFieldDelegate {
+    func textFieldDidBeginEditing(textField: UITextField) {
+        textField.placeholder = nil
+    }
 }
